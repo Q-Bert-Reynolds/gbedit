f4fdf3661c6e5a3ea573101d8920c5cf09c30ef8
Nolan Baker <nolan.lee.baker@gmail.com>
2019-11-11 23:21:47 -0600

    progress on encoder

--- png2gbdk.py
+++ png2gbdk.py
@@ -1,4 +1,5 @@
 import os
+import math
 from PIL import Image
 
 def main():
@@ -12,7 +13,31 @@ def main():
 
 def generate_c_file (base):
   img = Image.open(base + ".png")
+  
+  rows = int(img.height / 8)
+  cols = int(img.width / 8)
+  hex_vals = []
+  pixels = list(img.getdata())
+  for col in range(cols):
+    for row in range(rows):
+      for j in range(8):
+        upper_bin = ""
+        lower_bin = ""
+        for i in range(8):
+          x = col*8+i
+          y = row*8+j
+          px = int(math.floor(float(pixels[y*img.width+x][0]) / 64.0))
+          upper_bin += str(1-int(px/2))
+          lower_bin += str(1-int(px%2))
+        hex_vals.append(hex(int(upper_bin, 2)))
+        hex_vals.append(hex(int(lower_bin, 2)))
+  print(base)
+  print(hex_vals)
+  quit()
   c_file = open(base + ".c", "w+")
+  # write bytes here
+  c_file.close()
+
 
 if __name__ == "__main__":
     main()

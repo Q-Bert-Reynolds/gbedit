f2665764361d2b10da04ecf5850d05b669e2cf21
Nolan Baker <nolan.lee.baker@gmail.com>
2019-11-12 09:14:29 -0600

    encoder working, needs per-folder headers

--- png2gbdk.py
+++ png2gbdk.py
@@ -1,14 +1,19 @@
+#TODO: commandline options (1bpp, 2bpp, rle), header files for each folder
+
 import os
 import math
 from PIL import Image
 
 def main():
-  for root, dirs, files in os.walk("./res"):
+  for root, folders, files in os.walk("./res"):
     for name in files:
       path = os.path.join(root, name)
       base, ext = os.path.splitext(path)
       if ext == ".png":
         generate_c_file(base)
+    
+    for folder in folders:
+      print(folder) #create header files
 
 
 def generate_c_file (base):
@@ -18,26 +23,27 @@ def generate_c_file (base):
   cols = int(img.width / 8)
   hex_vals = []
   pixels = list(img.getdata())
-  for col in range(cols):
-    for row in range(rows):
+  for row in range(rows):
+    for col in range(cols):
       for j in range(8):
         upper_bin = ""
         lower_bin = ""
         for i in range(8):
           x = col*8+i
           y = row*8+j
-          px = int(math.floor(float(pixels[y*img.width+x][0]) / 64.0))
+          px = pixels[y*img.width+x]
+          px = px if isinstance(px, int) else px[0]
+          px = int(math.floor(float(px) / 64.0))
           upper_bin += str(1-int(px/2))
           lower_bin += str(1-int(px%2))
-        hex_vals.append(hex(int(upper_bin, 2)))
-        hex_vals.append(hex(int(lower_bin, 2)))
-  print(base)
-  print(hex_vals)
-  quit()
-  c_file = open(base + ".c", "w+")
-  # write bytes here
-  c_file.close()
+        hex_vals.append("0x{:02x}".format(int(upper_bin, 2)))
+        hex_vals.append("0x{:02x}".format(int(lower_bin, 2)))
 
+  with open(base + ".c", "w+") as c_file:
+    c_file.write("const unsigned char _" + os.path.basename(base) + "[] = {\n")
+    for i in range(0, len(hex_vals), 16):
+      c_file.write("    " + ",".join(hex_vals[i:i+16]) + ",\n")
+    c_file.write("}\n")
 
 if __name__ == "__main__":
     main()

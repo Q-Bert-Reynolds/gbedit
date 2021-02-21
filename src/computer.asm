380ffaeca6ad0799dbeb63706fdad97b8d22d1ab
Nolan Baker <nolan.lee.baker@gmail.com>
2021-02-20 19:31:32 -0600

    adds root menu to item storage system

--- src/computer.asm
+++ src/computer.asm
@@ -1,13 +1,10 @@
-TurnedOnComputerText: DB "%s turned on\nthe PC.", 0
-
-;Bedroom computer only shows items.
 ;Until you meet Bill James, it says "SOMEONE's PC"
+TurnedOnComputerText:          DB "%s turned on\nthe PC.", 0
+AccessedMyComputerText:        DB "Accessed my PC.", 0
+AccessedItemStorageSystemText: DB "Accessed Item\nStorage System.", 0
+WhatDoYouWantToDoText:         DB "What do you want\nto do?", 0
+ItemStorageSystemMenuText:        DB "WITHDRAW ITEM\nDEPOSIT ITEM\nTOSS ITEM\nLOG OFF", 0
 
-;ITEMS:
-; WITHDRAW ITEM
-; DEPOSIT ITEM
-; TOSS ITEM
-; LOG OFF
 
 ;BILL's PC
 ; "Accessed BILL's PC."
@@ -30,7 +27,20 @@ TurnedOnComputerText: DB "%s turned on\nthe PC.", 0
 ; LEAGUE
 ; "Accessed HALL OF FAME List."
 
-UseComputer:
+UsePublicComputer:
+  call ShowTurnedOnComputerText
+  HIDE_WIN
+  WAITPAD_UP
+  ret
+
+UseMyComputer:
+  call ShowTurnedOnComputerText
+  call AccessMyComputer
+  HIDE_WIN
+  WAITPAD_UP
+  ret
+
+ShowTurnedOnComputerText:
   ld hl, TurnedOnComputerText
   ld bc, user_name
   ld de, str_buffer
@@ -38,5 +48,37 @@ UseComputer:
   ld hl, str_buffer
   call RevealTextAndWait
   HIDE_WIN
-  WAITPAD_UP
   ret
+
+AccessMyComputer:
+  call CopyBkgToWin
+
+  ld bc, 12
+  ld hl, WhatDoYouWantToDoText
+  ld a, DRAW_FLAGS_PAD_TOP | DRAW_FLAGS_WIN
+  call DisplayTextAtPos
+  ld a, 7
+  ld [rWX], a
+  xor a
+  ld [rWY], a
+  SHOW_WIN
+
+  xor a
+  ld [list_selection], a
+.showItemStorageSystemMenu
+    ld hl, ItemStorageSystemMenuText
+    ld de, str_buffer
+    call str_Copy
+    xor a
+    ld [name_buffer], a
+    ld bc, 0
+    ld de, $100A
+    ld a, DRAW_FLAGS_PAD_TOP | DRAW_FLAGS_WIN
+    call ShowListMenu
+
+    cp a, 0
+    ret z
+    cp a, 4
+    ret z
+
+    jp .showItemStorageSystemMenu
